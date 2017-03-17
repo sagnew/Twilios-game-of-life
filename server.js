7df02c14a00060db2c0573d4d985f0c65548ef61
Sam Agnew <sagnew@eden.rutgers.edu>
2017-03-16 20:55:20 -0400

    Emit SMS messages to the client

--- server.js
+++ server.js
@@ -8,17 +8,18 @@ var bodyParser = require('body-parser');
 var twilio = require('twilio');
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(3000, function () {
+  console.log('Your app is listening on port ' + 3000);
 });
 
 var io = require('socket.io')(listener);
 
-// we've started you off with Express, 
+// we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
@@ -39,11 +40,15 @@ app.post('/sms', (req, res) => {
 
   // Generate a TwiML response
   var twiml = new twilio.TwimlResponse();
+  var msgBody = req.body.Body.trim();
 
   // Talk in a robot voice over the phone.
   twiml.message('Thanks for playing.');
-  
-  io.emit('create', req.body.Body);
+
+
+  // if (msgBody) {
+    io.emit('addPattern', msgBody);
+  // }
 
   // Set the response type as XML.
   res.header('Content-Type', 'text/xml');
@@ -52,10 +57,3 @@ app.post('/sms', (req, res) => {
   res.send(twiml.toString());
 
 });
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
